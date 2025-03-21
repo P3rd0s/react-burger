899866f4b6496d538a50cf9a402466902ae60b2c
Широв Александр Александрович <aashirov@domclick.ru>
2025-03-21 13:24:35 +0300

    configuration fixes

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -81,6 +81,8 @@ module.exports = {
 			'@components': path.resolve(__dirname, '..', './src/components'),
 			'@services': path.resolve(__dirname, '..', './src/services'),
 			'@utils': path.resolve(__dirname, '..', './src/utils'),
+			'@shared': path.resolve(__dirname, '..', './src/shared'),
+			'@assets': path.resolve(__dirname, '..', './src/assets'),
 		}
 	},
 	plugins: [
